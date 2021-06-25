d6f70cb9a8695662079754949cfa6993435acd6d
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-25 09:39:51 -0700

    add second failing test

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,8 +1,14 @@
 import {GalacticAge} from '../src/galactic.js'
 
 describe('GalacticAge', () => {
+  
   test('GalacticAge should have age property', () => {
     let newAge = new GalacticAge(50)
     expect(newAge.age).toBeDefined();
+  });
+
+  test('GalacticAge should have lifeExpectancy property', () => {
+    let newAge = new GalacticAge(50)
+    expect(newAge.lifeExpectancy).toBeDefined();
   });
 });
